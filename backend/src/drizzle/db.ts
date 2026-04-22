ef12d963a149e74b875b2fcbb12ae38075556931
lee-limo <lee-limo@kali.limo>
2026-04-22 11:19:07 +0300

    prepare backend for render deploy

--- backend/src/drizzle/db.ts
+++ backend/src/drizzle/db.ts
@@ -1,17 +1,38 @@
 import "dotenv/config";
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
-// Create PostgreSQL connection pool
+import * as schema from "@/drizzle/schema";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// Initialize Drizzle with schema
 const db = drizzle(pool, {
   schema,
-  logger: true, // set to false in production
+  logger: true,
 });
 
 export default db;
+
+// import "dotenv/config";
+// import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import * as schema from "./schema";
+
+// // Create PostgreSQL connection pool
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// // Initialize Drizzle with schema
+// const db = drizzle(pool, {
+//   schema,
+//   logger: true, // set to false in production
+// });
+
+// export default db;
