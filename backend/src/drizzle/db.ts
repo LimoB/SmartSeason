e07419e8161e4aad13088074d1f2b95b71a7c4be
lee-limo <lee-limo@kali.limo>
2026-04-22 11:40:35 +0300

    trigger deploy

--- backend/src/drizzle/db.ts
+++ backend/src/drizzle/db.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
-import * as schema from "@/drizzle/schema";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
